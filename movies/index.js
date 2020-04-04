96bfbf64964f32305f6b1864d913aa925b3c70b4
Pavel <Pawelv9@gmail.com>
2020-04-04 22:05:55 +0300

    added movie representation after click

--- movies/index.js
+++ movies/index.js
@@ -50,6 +50,12 @@ const onInput = async event => {
       ${movie.Title}
     `;
 
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    })
+
     resultsWrapper.appendChild(option)
   }
 
@@ -74,3 +80,33 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active');
   }
 });
+
+onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apiKey: API_KEY,
+      i: movie.imdbID
+    }
+  });
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+}
+
+const movieTemplate = movieDetail => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p>
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `;
+}
