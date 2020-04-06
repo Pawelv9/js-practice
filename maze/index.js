7e015b95e6759496b1665d45a3b84d80f0cf5bea
Pavel <Pawelv9@gmail.com>
2020-04-07 01:40:38 +0300

    refactoring

--- maze/index.js
+++ maze/index.js
@@ -1,9 +1,13 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
+const cellsHorizontal = 14;
+const cellsVertical = 10;
 const cells = 8;
-const width = 600;
-const height = 600;
-const unitLength = width / cells;
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
@@ -21,6 +25,7 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 // Walls 
+
 const walls = [
   Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
   Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
@@ -48,20 +53,20 @@ const shuffle = array => {
   return array;
 }
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false))
+  .map(() => Array(cellsHorizontal).fill(false))
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false))
+  .map(() => Array(cellsHorizontal - 1).fill(false))
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
   .fill(null)
-  .map(() => Array(cells).fill(false))
+  .map(() => Array(cellsHorizontal).fill(false))
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const stepThroughCell = (row, column) => {
   if (grid[row][column]) {
@@ -82,7 +87,11 @@ const stepThroughCell = (row, column) => {
   for (let neighbor of neighbors) {
     const [nextRow, nextColumn, direction] = neighbor;
 
-    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+    if (
+      nextRow < 0 || 
+      nextRow >= cellsVertical || 
+      nextColumn < 0 || 
+      nextColumn >= cellsHorizontal) {
       continue;
     }
 
@@ -112,9 +121,9 @@ horizontals.forEach((row, rowIndex) => {
     }
 
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
       5,
       {
         label: 'wall',
@@ -132,10 +141,10 @@ verticals.forEach((row, rowIndex) => {
     }
     
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
       5,
-      unitLength, 
+      unitLengthY, 
       {
         label: 'wall',
         isStatic: true
@@ -148,10 +157,10 @@ verticals.forEach((row, rowIndex) => {
 // Goal
 
 const goal = Bodies.rectangle(
-  width - unitLength / 2,
-  height - unitLength / 2,
-  unitLength * .7,
-  unitLength * .7,
+  width - unitLengthX / 2,
+  height - unitLengthY / 2,
+  unitLengthX * .7,
+  unitLengthY * .7,
   {
     isStatic: true,
     label: 'goal'
@@ -161,10 +170,11 @@ World.add(world, goal);
 
 //Ball
 
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(
-  unitLength / 2,
-  unitLength / 2,
-  unitLength / 4,
+  unitLengthX / 2,
+  unitLengthY / 2,
+  ballRadius,
   {
     label: 'ball'
   }
@@ -176,20 +186,19 @@ document.addEventListener('keydown', event => {
   
   if (event.keyCode === 87) {
     Body.setVelocity(ball,  { x, y: y - 5 });
-  }
+  };
   
   if (event.keyCode === 68) {
-    Body.setVelocity(ball,  { x: x + 5, y});    
-  }
+    Body.setVelocity(ball,  { x: x + 5, y });    
+  };
   
   if (event.keyCode === 83) {
     Body.setVelocity(ball,  { x, y: y + 5 });
-    
-  } 
+  };
   
   if (event.keyCode === 65) {
-    Body.setVelocity(ball,  { x: x - 5, y});    
-  } 
+    Body.setVelocity(ball,  { x: x - 5, y });    
+  };
 });
 
 // Win Condition
