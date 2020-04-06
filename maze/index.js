0714cdf27c410dc9b3b4ff85118b33004d556afa
Pavel <Pawelv9@gmail.com>
2020-04-06 14:32:59 +0300

    refactored and added constants for grid

--- maze/index.js
+++ maze/index.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 600;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,25 +9,32 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}))
-
 // Walls 
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false))
+
+const verticals = Array(3)
+  .fill(null)
+  .map(() => Array(2).fill(false))
+
+const horosontals = Array(2)
+  .fill(null)
+  .map(() => Array(3).fill(false))
